b0ece74ad6dfa0ec7d9da3e29a507a8df2a693b5
Nathee Phuwong <natee.live@gmail.com>
2024-09-30 11:26:30 +0700

    add new serviceflow modal empty

--- platform-master/src/sections/serviceflow/view/serviceflow-view.jsx
+++ platform-master/src/sections/serviceflow/view/serviceflow-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 import Card from "@mui/material/Card"
 import Stack from "@mui/material/Stack"
@@ -19,6 +19,7 @@ import ServiceFlowTableHead from "../serviceflow-table-head"
 import TableEmptyRows from "../table-empty-rows"
 import ServiceFlowTableToolbar from "../serviceflow-table-toolbar"
 import { emptyRows, applyFilter, getComparator } from "../utils"
+import NewServiceflowModal from "../modal"
 
 // ----------------------------------------------------------------------
 
@@ -29,6 +30,8 @@ export default function ServiceFlowPage() {
   const [orderBy, setOrderBy] = useState("name")
   const [filterName, setFilterName] = useState("")
   const [rowsPerPage, setRowsPerPage] = useState(5)
+
+  const [openModal, setOpenModal] = useState(false)
 
   const handleSort = (event, id) => {
     const isAsc = orderBy === id && order === "asc"
@@ -88,92 +91,96 @@ export default function ServiceFlowPage() {
   const notFound = !dataFiltered.length && !!filterName
 
   return (
-    <Container>
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        mb={5}
-      >
-        <Typography variant="h4">ServiceFlows</Typography>
-        <Button
-          variant="contained"
-          color="inherit"
-          startIcon={<Iconify icon="eva:plus-fill" />}
+    <React.Fragment>
+      <Container>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
         >
-          New ServiceFlow
-        </Button>
-      </Stack>
+          <Typography variant="h4">ServiceFlows</Typography>
+          <Button
+            variant="contained"
+            color="inherit"
+            startIcon={<Iconify icon="eva:plus-fill" />}
+            onClick={()=>setOpenModal(true)}
+          >
+            New ServiceFlow
+          </Button>
+        </Stack>
 
-      <Card>
-        <ServiceFlowTableToolbar
-          numSelected={selected.length}
-          filterName={filterName}
-          onFilterName={handleFilterByName}
-        />
+        <Card>
+          <ServiceFlowTableToolbar
+            numSelected={selected.length}
+            filterName={filterName}
+            onFilterName={handleFilterByName}
+          />
 
-        <Scrollbar>
-          <TableContainer sx={{ overflow: "unset" }}>
-            <Table sx={{ minWidth: 800 }}>
-              <ServiceFlowTableHead
-                order={order}
-                orderBy={orderBy}
-                rowCount={serviceflows.length}
-                numSelected={selected.length}
-                onRequestSort={handleSort}
-                onSelectAllClick={handleSelectAllClick}
-                headLabel={[
-                  { id: "id", label: "Id" },
-                  { id: "projectName", label: "Project Name" },
-                  { id: "serviceFlowName", label: "Serviceflow Name" },
-                  { id: "updatedDate", label: "Update Date" },
-                  { id: "version", label: "Version" },
-                  { id: "status", label: "Status" },
-                  { id: "" }
-                ]}
-              />
-              <TableBody>
-                {dataFiltered
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map(row =>
-                    <ServiceFlowTableRow
-                      key={row.id}
-                      id={row.id}
-                      projectName={row.projectName}
-                      serviceFlowName={row.serviceFlowName}
-                      createdDate={row.createdDate}
-                      updatedDate={row.updatedDate}
-                      version={row.version}
-                      manager={row.manager}
-                      status={row.status}
-                      projectUrl={row.projectUrl}
-                      serviceFlowUrl={row.serviceFlowUrl}
-                      handleClick={event =>
-                        handleClick(event, row.serviceFlowName)}
-                    />
-                  )}
-
-                <TableEmptyRows
-                  height={77}
-                  emptyRows={emptyRows(page, rowsPerPage, serviceflows.length)}
+          <Scrollbar>
+            <TableContainer sx={{ overflow: "unset" }}>
+              <Table sx={{ minWidth: 800 }}>
+                <ServiceFlowTableHead
+                  order={order}
+                  orderBy={orderBy}
+                  rowCount={serviceflows.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleSort}
+                  onSelectAllClick={handleSelectAllClick}
+                  headLabel={[
+                    { id: "id", label: "Id" },
+                    { id: "projectName", label: "Project Name" },
+                    { id: "serviceFlowName", label: "Serviceflow Name" },
+                    { id: "updatedDate", label: "Update Date" },
+                    { id: "version", label: "Version" },
+                    { id: "status", label: "Status" },
+                    { id: "" }
+                  ]}
                 />
+                <TableBody>
+                  {dataFiltered
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map(row =>
+                      <ServiceFlowTableRow
+                        key={row.id}
+                        id={row.id}
+                        projectName={row.projectName}
+                        serviceFlowName={row.serviceFlowName}
+                        createdDate={row.createdDate}
+                        updatedDate={row.updatedDate}
+                        version={row.version}
+                        manager={row.manager}
+                        status={row.status}
+                        projectUrl={row.projectUrl}
+                        serviceFlowUrl={row.serviceFlowUrl}
+                        handleClick={event =>
+                          handleClick(event, row.serviceFlowName)}
+                      />
+                    )}
 
-                {notFound && <TableNoData query={filterName} />}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Scrollbar>
+                  <TableEmptyRows
+                    height={77}
+                    emptyRows={emptyRows(page, rowsPerPage, serviceflows.length)}
+                  />
 
-        <TablePagination
-          page={page}
-          component="div"
-          count={serviceflows.length}
-          rowsPerPage={rowsPerPage}
-          onPageChange={handleChangePage}
-          rowsPerPageOptions={[5, 10, 25]}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Card>
-    </Container>
+                  {notFound && <TableNoData query={filterName} />}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            page={page}
+            component="div"
+            count={serviceflows.length}
+            rowsPerPage={rowsPerPage}
+            onPageChange={handleChangePage}
+            rowsPerPageOptions={[5, 10, 25]}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>
+      <NewServiceflowModal openModal={openModal} setOpenModal={setOpenModal} />
+    </React.Fragment>
   )
 }
